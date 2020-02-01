ee35435154d5ca67d77cb8e1be5f4638320ffd57
Rickard Laurin <rickard.laurin@gmail.com>
2020-02-01 09:36:04 +0100

    chore: test build

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,15 @@ import { SearchProvider } from './context/SearchContext'
 
 const MovieRoute = React.lazy(() => import('./pages/Movie'))
 
-const App = () => {
-  return (
-    <SearchProvider>
-      <React.Suspense fallback="">
-        <Router>
-          <Search path="/" />
-          <MovieRoute path="/movie/:id" />
-        </Router>
-      </React.Suspense>
-    </SearchProvider>
-  )
-}
+const App = () => (
+  <SearchProvider>
+    <React.Suspense fallback="">
+      <Router>
+        <Search path="/" />
+        <MovieRoute path="/movie/:id" />
+      </Router>
+    </React.Suspense>
+  </SearchProvider>
+)
 
 export default App
